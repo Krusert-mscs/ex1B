688eb99282d47b490022ab670e6c2f97feb04de6
Nathan Putz <Nathan Putz@DESKTOP-QSFFOU6>
2022-08-30 22:53:38 -0500

    Add project files.

---
+++ ex1B/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex1B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal subtotal = Convert.ToDecimal(txtsubtotal.Text);
+            decimal discountPercent = 0m;
+            if (subtotal >= 500)
+            {
+                discountPercent = .2m;
+            }
+            else if (subtotal >= 250 && subtotal < 500;)
+            {
+                discountPercent = .15m;
+            }
+            else if (subtotal >= 100 && subtotal < 250)
+            {
+                discountPercent = .1m;
+            }
+            decimal discountAmount = subtotal * discountPercent;
+            decimal invoiceTotal = subtotal - discountAmount;
+
+            txtDiscountPercent.Text = discountPercent.ToString("p1");
+            txtDiscountAmount.Text = discountAmount.ToString("c");
+            txtTotal.Text = invoiceTotal.ToString("c");
+
+            txtsubtotal.Focus();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
